46e6ae5f2716f52d9e46d747b8963e10f6fb7b74
rfraposa <richraposa@gmail.com>
2018-01-18 07:36:56 -0700

    Added blogs.sql

--- db_node/conf/blogs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
-    create table blogs(title text, seo_title text, url text, author text, dateposted date, category text, locales text, content text);
-	copy blogs from '/docker-entrypoint-initdb.d/blogs.csv' DELIMITERS ';' CSV;
-EOSQL

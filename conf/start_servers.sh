b474d273747e7542bc08ae5995a6f7e33ed9551a
rfraposa <richraposa@gmail.com>
2018-01-31 20:42:51 -0700

    Exposed port 9200

--- conf/start_servers.sh
+++ conf/start_servers.sh
@@ -7,12 +7,12 @@ echo "Starting $1 servers..."
 for (( i=1; i<=$1; ++i));
 do
 ip=$((i+1))
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip  -i -t elastic/server)
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200  -i -t elastic/server)
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
 echo "Started server$i on IP $server_ip"
 done
 
 echo "Starting the database server"
-docker run --restart always --privileged --dns 8.8.8.8 --name db_server -h db_server --ip 172.18.0.30 --net=es_bridge -e POSTGRES_PASSWORD=password -d elastic/db_server
+docker run --restart always --privileged --dns 8.8.8.8 --name db_server -h db_server --ip 172.18.0.30  --net=es_bridge -e POSTGRES_PASSWORD=password -d elastic/db_server
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' db_server)
 echo "Database server running on IP $server_ip"

aea59569f3d550181b571b96c453f575df674daa
rfraposa <richraposa@gmail.com>
2018-02-18 10:15:07 -0700

    Added shared volume to containers

--- conf/start_servers.sh
+++ conf/start_servers.sh
@@ -6,7 +6,7 @@ rm -rf /home/elastic/.ssh/known_hosts
 echo "Starting $1 servers..."
 
 ip=2
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server1 -h server1 --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200 -p 5601:5601 -i -t elastic/server1)
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server1 -h server1 --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200 -p 5601:5601 -v es-vol:/shared_folder -i -t elastic/server1)
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server1)
 echo "Started server1 on IP $server_ip"
 
@@ -14,7 +14,7 @@ echo "Started server1 on IP $server_ip"
 for (( i=2; i<=$1; ++i));
 do
 ip=$((i+1))
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip  -i -t elastic/server)
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -v es-vol:/shared_folder  -i -t elastic/server)
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
 echo "Started server$i on IP $server_ip"
 done

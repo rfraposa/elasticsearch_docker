131191c760ba19c1b89d5c327a22cb6e73685a47
rfraposa <richraposa@gmail.com>
2018-03-24 08:36:46 -0600

    Trying to fix port issue

--- conf/eng2_servers.sh
+++ conf/eng2_servers.sh
@@ -5,10 +5,15 @@ rm -rf /home/elastic/.ssh/known_hosts
 #Start the containers
 echo "Starting 5 servers for Engineer II..."
 
-for (( i=1; i<=3; ++i));
+i=1
+ip=2
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200 -p 5601:5601 -v es-vol:/shared_folder -i -t elastic/node$i)
+server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
+
+for (( i=2; i<=3; ++i));
 do
 ip=$((i+1))
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip  -v es-vol:/shared_folder -i -t elastic/node$i)
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200 -p 5601 -v es-vol:/shared_folder -i -t elastic/node$i)
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
 echo "Started server$i on IP $server_ip"
 done
@@ -16,7 +21,7 @@ done
 for (( i=4; i<=5; ++i));
 do
 ip=$((i+1))
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip  -v es-vol:/shared_folder  -i -t elastic/eng2)
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200 -p 5601 -v es-vol:/shared_folder  -i -t elastic/eng2)
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
 echo "Started server$i on IP $server_ip"
 done

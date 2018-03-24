12eaf540abbd0fe37e60d1a11f9ca6c87d69a23e
rfraposa <richraposa@gmail.com>
2018-03-24 08:21:52 -0600

    Updated startup script

--- conf/eng2_servers.sh
+++ conf/eng2_servers.sh
@@ -3,18 +3,20 @@
 rm -rf /home/elastic/.ssh/known_hosts
 
 #Start the containers
-echo "Starting $1 servers..."
+echo "Starting 5 servers for Engineer II..."
 
-ip=2
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server1 -h server1 --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200 -p 5601:5601 -v es-vol:/shared_folder -i -t elastic/eng2)
-server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server1)
-echo "Started server1 on IP $server_ip"
-
-
-for (( i=2; i<=$1; ++i));
+for (( i=1; i<=3; ++i));
 do
 ip=$((i+1))
-CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -v es-vol:/shared_folder  -i -t elastic/server)
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200 -p 5601:5601 -v es-vol:/shared_folder -i -t elastic/node$i)
+server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
+echo "Started server$i on IP $server_ip"
+done
+
+for (( i=4; i<=5; ++i));
+do
+ip=$((i+1))
+CID=$(docker run -d --restart always --privileged --dns 8.8.8.8  --name server$i -h server$i --publish-all=true -d  --net=es_bridge --ip 172.18.0.$ip -p 9200:9200 -p 5601:5601 -v es-vol:/shared_folder  -i -t elastic/eng2)
 server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server$i)
 echo "Started server$i on IP $server_ip"
 done
